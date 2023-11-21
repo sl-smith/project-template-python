5f6e1dca286574a976d149e347eb1dcf90e0a4fe
Shawn Smith <ssmith161803@gmail.com>
2023-11-20 21:54:31 -0700

    fix(ci): added type hints to conftest

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,8 +1,10 @@
 """Configuration for the pytest test suite."""
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from _pytest.main import Session
 
-def pytest_sessionfinish(session, exitstatus):
-    """
-    avoids issue of no tests causing gh action failure
-    """
+
+def pytest_sessionfinish(session: Session, exitstatus: int) -> None:
+    """avoids issue of no tests causing gh action failure"""
     if exitstatus == 5:
         session.exitstatus = 0
