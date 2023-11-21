ba122ef4cdcbbf8c4fa336f185de35de3698fd65
Shawn Smith <ssmith161803@gmail.com>
2023-11-20 22:10:02 -0700

    fix(ci): sorting type hint errors

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,10 +1,8 @@
 """Configuration for the pytest test suite."""
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from _pytest.main import Session
+from pytest import Session
 
 
 def pytest_sessionfinish(session: Session, exitstatus: int) -> None:
-    """avoids issue of no tests causing gh action failure"""
+    """Avoids issue of no tests causing gh action failure."""
     if exitstatus == 5:
         session.exitstatus = 0
