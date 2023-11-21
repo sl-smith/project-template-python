84b415fb9d3005dfe5735994d14ffc3f1ac50011
Shawn Smith <ssmith161803@gmail.com>
2023-11-20 21:43:15 -0700

    fix(ci): added function to prevent pytest failure

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,1 +1,8 @@
 """Configuration for the pytest test suite."""
+
+def pytest_sessionfinish(session, exitstatus):
+    """
+    avoids issue of no tests causing gh action failure
+    """
+    if exitstatus == 5:
+        session.exitstatus = 0
